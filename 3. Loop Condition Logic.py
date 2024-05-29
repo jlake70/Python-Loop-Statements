f60d367efdd88327cce526bc09278cb968c092d5
jlake70 <jlake7021@gmail.com>
2024-05-29 12:13:09 -0400

    Completed question 3

---
+++ 3. Loop Condition Logic.py
@@ -0,0 +1,17 @@
+# Task 1
+
+counter = 0
+
+while True:
+    counter += 1
+    print(counter)
+
+    if counter >= 5:
+        break
+
+# Task 2
+
+while counter < 5:
+    counter += 1
+
+    print(counter)
