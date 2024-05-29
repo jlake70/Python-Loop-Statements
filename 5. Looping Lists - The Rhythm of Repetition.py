e7c09699fe75b0e5b84d02d69c3c4bc4a9037d98
jlake70 <jlake7021@gmail.com>
2024-05-29 19:27:58 -0400

    Need to modify to make print statement iterate through genres list until classical is met

---
+++ 5. Looping Lists - The Rhythm of Repetition.py
@@ -0,0 +1,32 @@
+# Task 1
+
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+counter = 0
+track_message = "Track"
+track_counter = 0
+
+
+for genre in genres:
+    print(f"My favorite type of music to listen to is {genre}")
+
+
+# Task 2
+for genre in genres:
+    counter += 1
+    print(f" {track_message} {counter} : {genre}")
+    
+    
+
+# Task 3
+
+for index in range(len(genres)):
+   
+        if genres[index] == "Classical":
+            continue     
+        else:
+            for genre in genres[index]:
+                    track_counter += 1    
+    
+                    print(f"{track_message} {track_counter} : {genres[index]} is ready for the light show")
+            break
+   
